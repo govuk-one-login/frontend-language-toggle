ab7b3780173c1a7d566e7f6a0239cfee2edd5374
SophiaDWokoma <148252375+SophiaDWokoma@users.noreply.github.com>
2024-05-03 13:52:19 +0100

    add files to repo

---
+++ src/language-param-setter.test.ts
@@ -0,0 +1,37 @@
+import addLanguageParam from "./language-param-setter";
+
+describe("addLanguageParam function", () => {
+  it("should add language parameter to URL", () => {
+    const language = "en";
+    const url = "http://example.com";
+
+    const result = addLanguageParam(language, url);
+    expect(result).toContain("lng=en");
+  });
+
+  it("should add language parameter to URL without any existing parameters", () => {
+    const result = addLanguageParam("en", "http://example.com");
+    expect(result).toContain("/?lng=en");
+  });
+
+  it("should add language parameter to URL with existing parameters", () => {
+    const result = addLanguageParam(
+      "en",
+      "http://localhost:6001/path?param1=value1"
+    );
+    expect(result).toContain("/path?");
+    expect(result).toContain("lng=en");
+  });
+  ``;
+
+  it("should not duplicate the language parameter in URL", () => {
+    const result = addLanguageParam(
+      "cy",
+      "http://localhost:6001/path?param1=value1&lng=en"
+    );
+    expect(result).toContain("/path?");
+    expect(result).toContain("param1=value1");
+    expect(result).toContain("lng=cy");
+    expect(result.split("lng=").length - 1).toEqual(1); // ensures 'lng' parameter appears only once
+  });
+});
---
+++ src/language-param-setter.ts
@@ -0,0 +1,5 @@
+export default function addLanguageParam(language: string, url: string) {
+  const parsedUrl = new URL(url);
+  parsedUrl.searchParams.set("lng", language);
+  return parsedUrl.pathname + parsedUrl.search;
+}
